b46ed4a37d340a6d4030e7720c60311a8ab1c9f1
SashasChallenge <2039927213@qq.com>
2026-04-13 22:15:23 +0800

    Delete molecue dynamycss directory

--- molecue dynamycss/docking/PARP1-Veliparib/1.0-Docking.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for %%f in (Veliparib.pdbqt) do (
-echo Processing %%f
-mkdir %%f_result
-"C:\Program Files (x86)\The Scripps Research Institute\Vina\vina.exe" --config conf.txt --ligand %%f --out %%f_result\%%f_out.pdb --log %%f_result\%%f_log.txt
-)
-echo 任务完成！ 
-pause&exit
